05be3fed6f497a8bcf6f72ef83800775bcd29491
SebOchoa8a <sebochoa8a@gmail.com>
2024-03-25 23:21:48 -0600

    some history.c

---
+++ src/history.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "history.h"
+
+#define MAXTOKENS 100
+
+/* Initialize the linked list to keep the history. */
+
+List* init_history()
+{
+  List *token_history = (List*)malloc(sizeof(List));
+  if(token_history != NULL)
+    {
+      history ->root = NULL;
+    }
+  return history;
+}
+
+/* Add a history item to the end of the list.
+   List* list - the linked list
+   char* str - the string to store
+*/
+void add_history(List *list, char *str)
+{
+  
+}
+
+/* Retrieve the string stored in the node where Item->id == id.
+   List* list - the linked list
+   int id - the id of the Item to find */
+
+char *get_history(List *list, int id);
+
+/*Print the entire contents of the list. */
+
+void print_history(List *list)
+{
+}
+
+/*Free the history list and the strings it references. */
+
+void free_history(List *list)
+{
+}
